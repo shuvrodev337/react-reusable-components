ae642b2f5e7c9db28a4a5886e61ec3ce4eead81b
unknown <shuvrodevmondal337@gmail.com>
2024-01-14 21:44:00 +0600

    first-commit

---
+++ src/components/layout/MainLayout.tsx
@@ -0,0 +1,5 @@
+const MainLayout = () => {
+  return <div>This is Main Layout</div>;
+};
+
+export default MainLayout;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div>This Is Home</div>;
+};
+
+export default Home;
